3e7e10dd9592face2e5b813f4a95c911bfa387c6
Szomor János <jszomor@gmail.com>
2019-10-04 15:02:33 +0200

    FindJsonKey added

---
+++ EquipmentPosition/EquipmentPosition/EquipmentSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentPosition
+{
+  class EquipmentSelector
+  {
+    public void EqPumpSelect()
+    {
+      var selectorProperty = new SelectorProperty();
+
+      selectorProperty.numberOfEqipment = (selectorProperty.avgDailyFlow / 24) / selectorProperty.capacity;
+      
+    }
+  }
+}
---
+++ EquipmentPosition/EquipmentPosition/SelectorProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentPosition
+{
+  class SelectorProperty
+  {
+    public double avgDailyFlow { get; set; }
+    public double capacity { get; set; }
+    public double head { get; set; }
+    public double numberOfEqipment { get; set; }
+    public bool eqPump { get; set; }
+    public bool eqLIT { get; set; }
+    public bool eqFIT { get; set; }
+    public bool eqJet { get; set; }
+    public bool eqMixer { get; set; }
+    
+  }
+}
