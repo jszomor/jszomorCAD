a6533d1915d36fe2cece6538aefc233d8d7ba077
Szomor János <jszomor@gmail.com>
2019-10-25 16:06:28 +0200

    EqPumpSelect logic changed

--- EquipmentPosition/EquipmentPosition/EquipmentSelector.cs
+++ EquipmentPosition/EquipmentPosition/EquipmentSelector.cs
@@ -19,43 +19,39 @@ namespace EquipmentPosition
       
       try
       {
-        if (selectorProperty.AvgHourlyFlow >= 0 && selectorProperty.AvgHourlyFlow <= 400)
+        if (selectorProperty.AvgHourlyFlow <= 800)
         {
-          selectorProperty.Capacity = 400;
+          selectorProperty.NumberOfEqipment = 1;
         }
-        else if (selectorProperty.AvgHourlyFlow >= 401 && selectorProperty.AvgHourlyFlow <= 800)
-        {
-          selectorProperty.Capacity = 600;
-        }
-        else if (selectorProperty.AvgHourlyFlow >= 801 && selectorProperty.AvgHourlyFlow <= 1200)
-        {
-          selectorProperty.Capacity = 800;
-        }
-        else if (selectorProperty.AvgHourlyFlow >= 1201 && selectorProperty.AvgHourlyFlow <= 2000)
-        {
-          selectorProperty.Capacity = 1000;
-        }
-        else if (selectorProperty.AvgHourlyFlow >= 2001 && selectorProperty.AvgHourlyFlow <= 4000)
-        {
-          selectorProperty.Capacity = 1200;
-        }
-        else if (selectorProperty.AvgHourlyFlow >= 4001 && selectorProperty.AvgHourlyFlow <= 6000)
-        {
-          selectorProperty.Capacity = 1500;
-        }
-        else if (selectorProperty.AvgHourlyFlow >= 6001 && selectorProperty.AvgHourlyFlow <= 8500)
-        {
-          selectorProperty.Capacity = 2000;
-        }
+        else
+        {          
+          if (selectorProperty.AvgHourlyFlow >= 801 && selectorProperty.AvgHourlyFlow <= 1200)
+          {
+            selectorProperty.Capacity = 800;
+          }
+          else if (selectorProperty.AvgHourlyFlow >= 1201 && selectorProperty.AvgHourlyFlow <= 2000)
+          {
+            selectorProperty.Capacity = 1000;
+          }
+          else if (selectorProperty.AvgHourlyFlow >= 2001 && selectorProperty.AvgHourlyFlow <= 4000)
+          {
+            selectorProperty.Capacity = 1200;
+          }
+          else if (selectorProperty.AvgHourlyFlow >= 4001 && selectorProperty.AvgHourlyFlow <= 6000)
+          {
+            selectorProperty.Capacity = 1500;
+          }
+          else if (selectorProperty.AvgHourlyFlow >= 6001 && selectorProperty.AvgHourlyFlow <= 8500)
+          {
+            selectorProperty.Capacity = 2000;
+          }
+          selectorProperty.NumberOfEqipment = selectorProperty.AvgHourlyFlow / selectorProperty.Capacity;
+        }        
       }
       catch (Exception)
       {
         Console.WriteLine("out of range");
       }
-
-      selectorProperty.NumberOfEqipment = selectorProperty.AvgHourlyFlow / selectorProperty.Capacity;
-      //selectorProperty.NumberOfEqipment = Math.Round(selectorProperty.AvgHourlyFlow / selectorProperty.Capacity, 1, MidpointRounding.ToEven);
-      //selectorProperty.NumberOfEqipment = selectorProperty.AvgHourlyFlow / 40;
       return selectorProperty.NumberOfEqipment;      
     }
   }
