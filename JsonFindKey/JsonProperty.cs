e88c56c4c2dd3ea394e7ba42376bef532722efb7
Szomor János <jszomor@gmail.com>
2019-12-03 15:10:57 +0100

    add Organicad.Autocad

---
+++ JsonFindKey/JsonProperty.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonFindKey
+{
+  public class Position
+  {
+    public double X { get; set; }
+    public double Y { get; set;  }
+  }
+  public class JsonProperty
+  {
+    [JsonProperty("BlockName")]
+    public string Error { get; set; }
+  }
+}
