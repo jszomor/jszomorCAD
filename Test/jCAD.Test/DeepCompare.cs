0da88438f09d841977bfcdc6105d0f5a665939b9
János Szomor <jszomor@organicawater.com>
2020-02-09 20:27:06 +0100

    deep compare added

---
+++ Test/jCAD.Test/DeepCompare.cs
@@ -0,0 +1,88 @@
+﻿using JsonParse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jCAD.Test
+{
+  public static class DeepEx
+  {
+    public static void GetAttributeTextString(JsonBlockProperty block1, JsonBlockProperty block2)
+    {
+      //System.Diagnostics.Debug.WriteLine($"AutoCAD TAG: {attRef.Tag}");
+      var properties1 = block1.Attributes.GetType().GetProperties();
+      var properties2 = block2.Attributes.GetType().GetProperties();
+
+      if (properties1.Length != properties2.Length) return;
+
+      for (int i = 0; i < properties1.Length; i++)
+      {
+        var customAttributes = properties1[i]
+          .GetCustomAttributes(typeof(Newtonsoft.Json.JsonPropertyAttribute), false);
+        if (customAttributes.Length == 1)
+        {
+          var jsonProp = customAttributes[0];
+          var jsonTagName = (jsonProp as Newtonsoft.Json.JsonPropertyAttribute).PropertyName;
+          //System.Diagnostics.Debug.WriteLine($"\tJSONProperty Name: {jsonTagName}");
+          if (jsonTagName != null)
+          {
+            var propValue1 = properties1[i].GetValue(block1.Attributes);
+            var propValue2 = properties2[i].GetValue(block2.Attributes);
+            if (propValue1 == null) continue;
+            if (!string.IsNullOrWhiteSpace(propValue1.ToString()))
+            {
+              if (propValue1 == propValue2)
+              {
+                Console.WriteLine("win");
+              }
+            }
+
+            //prop.SetValue(block.Attributes, attRef.TextString); //serialization
+          }
+        }
+      }
+    }
+
+    public static bool DeepCompare(this object obj, object another)
+    {
+      if (ReferenceEquals(obj, another)) return true;
+      if ((obj == null) || (another == null)) return false;
+      //Compare two object's class, return false if they are difference
+      if (obj.GetType() != another.GetType()) return false;
+
+      var result = true;
+      //Get all properties of obj
+      //And compare each other
+      foreach (var property in obj.GetType().GetProperties())
+      {
+        var objValue = property.GetValue(obj);
+        var anotherValue = property.GetValue(another);
+        if (!objValue.Equals(anotherValue)) result = false;
+      }
+
+      return result;
+    }
+
+    public static bool CompareEx(this object obj, object another)
+    {
+      if (ReferenceEquals(obj, another)) return true;
+      if ((obj == null) || (another == null)) return false;
+      if (obj.GetType() != another.GetType()) return false;
+
+      //properties: int, double, DateTime, etc, not class
+      if (!obj.GetType().IsClass) return obj.Equals(another);
+
+      var result = true;
+      foreach (var property in obj.GetType().GetProperties())
+      {
+        var objValue = property.GetValue(obj);
+        var anotherValue = property.GetValue(another);
+        //Recursion
+        if (!objValue.DeepCompare(anotherValue)) result = false;
+      }
+      return result;
+    }
+  }
+}
---
+++ Test/jCAD.Test/TestProperty.cs
@@ -0,0 +1,71 @@
+﻿using JsonFindKey;
+using JsonParse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jCAD.Test
+{
+  class TestProperty
+  {
+  //  public List<TestJsonBlockProperty> Blocks { get; } = new List<TestJsonBlockProperty>();
+  //  public List<JsonLineProperty> Lines { get; } = new List<JsonLineProperty>();
+
+  //  //[Obsolete("Use BlocksSearch method instead, as it returns multiple lements with the same name")]
+  //  //public JsonBlockProperty BlockSearch(string equipmentName) => 
+  //  //  Blocks.SingleOrDefault(b => b.Misc.BlockName == equipmentName);
+
+  //  public bool IsEqual(TestProperty other)
+  //  {
+
+  //    for (int i = 0; i < Blocks.Count; i++)
+  //    {
+  //      if (!Blocks[i].IsIdentical(other.Blocks[i]))
+  //        return false;
+  //    }
+
+  //    for (int i = 0; i < Lines.Count; i++)
+  //    {
+  //      if (!Lines[i].IsIdentical(other.Lines[i]))
+  //        return false;
+  //    }
+
+  //    return true;
+  //  }
+
+  //  public IEnumerable<TestJsonBlockProperty> InternalIdSearch(int internalId) =>
+  //    Blocks.Where(b => b.Attributes.Contains(internalId));
+  //}
+  //public class TestJsonBlockProperty : IComparable<TestJsonBlockProperty>
+  //{
+  //  public IEnumerable<Geometry> Geometries { get; }
+  //  public IEnumerable<Misc> Miscs { get; }
+  //  public IEnumerable<General> Generals { get; }
+  //  public IEnumerable<Custom> Customs { get; }
+  //  public IEnumerable<Attributes> Attributes { get; }
+
+  //  public bool IsIdentical(TestJsonBlockProperty jsonBlockProperty)
+  //  {
+  //    //foreach (var i in Geometries)
+  //    //{
+
+  //    //}
+  //    //throw new NotImplementedException();
+  //    return true;
+  //  }
+
+
+  //  public int CompareTo(TestJsonBlockProperty comparePart)
+  //  {
+  //    // A null value means that this object is greater.
+  //    if (comparePart == null)
+  //      return 1;
+
+  //    else
+  //      return Attributes.Internal_Id.CompareTo(comparePart.Attributes.Internal_Id);
+  //  }
+  }
+
+}
